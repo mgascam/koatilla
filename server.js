83d4295b3450f3bb056786b2508ae92f2de1e713
Miguel Gasca <miguelgasca@gmail.com>
2019-10-06 00:36:30 +0200

    Initial

---
+++ server.js
@@ -0,0 +1,41 @@
+const Koa = require('koa');
+const proxy = require('koa-proxy');
+const cors = require('@koa/cors');
+const zlib = require('zlib');
+
+const fs = require('fs');
+
+const bodyParser = require('koa-body-parser');
+
+const app = new Koa();
+
+app.use(cors({origin: '*'}));
+app.use(bodyParser());
+
+// logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+app.use(async (ctx, next) => {
+  await next();
+  zlib.brotliDecompress(ctx.response.body, (err, result) => {
+    console.log(result.toString('utf8'))
+  })
+});
+
+app.use(proxy({
+  host: 'https://pokeapi.co'
+}));
+
+app.listen(3000);
